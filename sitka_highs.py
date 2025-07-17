bda539759aca2cf92c9f504f6c51b1f47ebcfcaf
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-17 18:07:51 +0300

    Extract high tempratures

--- sitka_highs.py
+++ sitka_highs.py
@@ -6,4 +6,11 @@ lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
-print(header_row)
+
+# Extract high temperatures.
+highs = []
+for row in reader:
+    high = int(row[4])
+    highs.append(high)
+
+print(highs)
