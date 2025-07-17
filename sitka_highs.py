1347bc790911d22a87dd5cf859a307f2811ac194
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-17 22:14:44 +0300

    Extract dates and high teperatures

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import csv
+from datetime import datetime
 
 import matplotlib.pyplot as plt
 
@@ -9,20 +10,23 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# Extract high temperatures.
-highs = []
+# Extract dates and high temperatures.
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
 # plot the high temperatures.
 plt.style.use('_classic_test_patch')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
 
 # Format plot.
 ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
 ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperatures (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
