0cd46fad5ef58783623f9d9daae46627c18639ec
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-17 21:00:50 +0300

    Plot the high temperatures

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import matplotlib.pyplot as plt
+
 path = Path('sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
 
@@ -13,4 +15,15 @@ for row in reader:
     high = int(row[4])
     highs.append(high)
 
-print(highs)
+# plot the high temperatures.
+plt.style.use('_classic_test_patch')
+fig, ax = plt.subplots()
+ax.plot(highs, color='red')
+
+# Format plot.
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel("", fontsize=16)
+ax.set_ylabel("Temperatures (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
