581786a65407135d26e20960ce4215cbcad05d3d
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-18 16:06:26 +0300

    Fix the bug for date with no value

--- Death/death_valley_highs_lows.py
+++ Death/death_valley_highs_lows.py
@@ -15,11 +15,15 @@ dates, highs, lows = [], [], []
 
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
-    high = int(row[3])
-    low = int(row[4])
-    dates.append(current_date)
-    highs.append(high)
-    lows.append(low)
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
 
 # Plot the high and low temperatures.
 plt.style.use("seaborn-v0_8-pastel")
@@ -29,7 +33,8 @@ ax.plot(dates, lows, color='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
+title = "Daily High and Low Temperatures, 2021\n Death Valley, CA"
+ax.set_title(title, fontsize=20)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperatures (F)", fontsize=16)
